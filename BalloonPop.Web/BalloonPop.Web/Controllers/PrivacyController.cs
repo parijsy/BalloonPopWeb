9fd52ad9036dd0cde5ccd6925e236b482cad8547
VSP\parijsy <y.parijs@loket.nl>
2019-06-13 19:53:03 +0200

    Added swizzy privacy policy

--- BalloonPop.Web/BalloonPop.Web/Controllers/PrivacyController.cs
+++ BalloonPop.Web/BalloonPop.Web/Controllers/PrivacyController.cs
@@ -11,6 +11,11 @@ namespace BalloonPop.Web.Controllers
             return View();
         }
 
+        public IActionResult Swizzy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
